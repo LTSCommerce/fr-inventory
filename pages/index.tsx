8ef63ab4d7be3b6f94cb15083aeae699ebb8c29e
Joseph Edmonds <joseph@ltscommerce.dev>
2022-12-20 18:50:58 +0000

    basic table now displaying, dates being formatted, rows are editable but it doesnt actually do anything just yet

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 // import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Box from '@mui/material/Box';
-import {DataGrid} from '@mui/x-data-grid';
+import {DataGrid, GridColumns} from '@mui/x-data-grid';
 import {
     PrismaClient,
     // Prisma,
@@ -12,42 +12,40 @@ import {
 } from "@prisma/client";
 import {InferGetStaticPropsType} from 'next'
 
-const columns: GridColDef[] = [
-    {field: 'id', headerName: 'ID', width: 90},
+const columns: GridColumns = [
+    {
+        field: 'id',
+        headerName: 'ID',
+        width: 90
+    },
+    {
+        field: 'name',
+        headerName: 'Name',
+        editable: true,
+        flex: 1,
+        minWidth: 150,
+    },
+    {
+        field: 'callsign',
+        headerName: 'Call Sign',
+        flex: 1,
+        minWidth: 150,
+    },
     {
         field: 'createdAt',
         headerName: 'Created',
         width: 150,
         editable: false,
+        valueFormatter: params => new Date(params?.value)
     },
     {
         field: 'updatedAt',
         headerName: 'Updated',
         width: 150,
         editable: false,
-    },
-    {
-        field: 'name',
-        headerName: 'Name',
-        editable: true,
-    },
-    {
-        field: 'callsign',
-        headerName: 'Call Sign'
+        valueFormatter: params => new Date(params?.value)
     },
 ];
-
-// const rows = [
-//     {id: 1, lastName: 'Snow', firstName: 'Jon', age: 35},
-//     {id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42},
-//     {id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45},
-//     {id: 4, lastName: 'Stark', firstName: 'Arya', age: 16},
-//     {id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null},
-//     {id: 6, lastName: 'Melisandre', firstName: null, age: 150},
-//     {id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44},
-//     {id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36},
-//     {id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65},
-// ];
 
 export const getStaticProps = async () => {
     const prisma = new PrismaClient();
@@ -64,7 +62,7 @@ export default function Home({data}: InferGetStaticPropsType<typeof getStaticPro
     return (
         <>
             <Head>
-                <title>Infrastructure</title>
+                <title>Inventory</title>
                 <meta name="description" content=""/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 {/*<link rel="icon" href="/favicon.ico" />*/}
